372d73c5259d613fa8149b69e225a1ad47f6ff68
webatics <femmor2u@gmail.com>
2017-06-08 07:49:28 +0100

    Practical JavaScript
    Practical JavaScript Lessons by Gordon Zhu

---
+++ js/script.js
@@ -0,0 +1,141 @@
+var todoList = {
+	todos: [],
+
+	// Method to display Todos
+	// displayTodos: function() {
+	// 	// If there are no todos
+	// 	if (this.todos.length === 0) {
+	// 		console.log('Your todo list is empty!');
+	// 	} else {
+	// 		console.log('My Todos: ');
+	// 		for (var i = 0; i < this.todos.length; i++) {
+	// 			if (this.todos[i].completed === true) {
+	// 				console.log('(x)', this.todos[i].todoText);
+	// 			} else {
+	// 				console.log('( )', this.todos[i].todoText);
+	// 			}
+
+	// 		// print with (x)
+	// 		// else 
+	// 		// print with ( )
+	// 		}
+	// 	}
+	// },
+
+	// Method to add Todos
+	addTodo: function(todoText) {
+		this.todos.push({
+			todoText: todoText,
+			completed: false
+		});
+		// this.displayTodos();
+	},
+
+	// Method to edit or change Todos
+	changeTodo: function(position, todoText) {
+		this.todos[position].todoText = todoText;
+		// this.displayTodos();
+	},
+
+	// Method to delete Todos
+	deleteTodo: function(position) {
+		this.todos.splice(position, 1);
+		// this.displayTodos();
+	},
+
+	// Method to toggle completed todos Todos
+	toggleCompleted: function(position) {
+		var todo = this.todos[position];
+		todo.completed = !todo.completed;
+		// this.displayTodos();
+	},
+
+	toggleAll: function() {
+		var totalTodos = this.todos.length;
+		var completedTodos = 0;
+
+		// Get number of completed todos
+		for (var i = 0; i < totalTodos; i++) {
+			if (this.todos[i].completed === false) {
+				completedTodos++;
+			}
+		}
+
+		// Case 1: If everything is true, make everything false
+		if (completedTodos === totalTodos) {
+			for (i = 0; i < totalTodos; i++) {
+				this.todos[i].completed === false;
+			}
+		} else {
+		// Case 2: Make everything true
+			for (i = 0; i < totalTodos; i++) {
+				this.todos[i].completed === true;
+			}
+		}
+		// this.displayTodos();
+	}
+};
+
+// Get access to the display todos button
+var handlers = {
+	// displayTodos: function() {
+	// 	todoList.displayTodos();
+	// },
+
+	addTodo: function() {
+		var addTodoTextInput = document.getElementById('addTodoTextInput');
+		todoList.addTodo(addTodoTextInput.value);
+		addTodoTextInput.value = '';
+		view.displayTodos();
+	},
+
+	changeTodo: function() {
+		var changeTodoPositionInput = document.getElementById('changeTodoPositionInput');
+		var changeTodoTextInput = document.getElementById('changeTodoTextInput');
+		todoList.changeTodo(changeTodoPositionInput.valueAsNumber, changeTodoTextInput.value);
+		changeTodoPositionInput.value = '';
+		changeTodoTextInput.value = '';
+		view.displayTodos();
+	},
+
+	deleteTodo: function() {
+		var deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
+		todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
+		deleteTodoPositionInput.value = '';
+		view.displayTodos();
+	},
+
+	toggleCompleted: function() {
+		var toggleCompletedTodoPositionInput = document.getElementById('toggleCompletedTodoPositionInput');
+		todoList.toggleCompleted(toggleCompletedTodoPositionInput.valueAsNumber);
+		toggleCompletedTodoPositionInput.value = '';
+		view.displayTodos();
+	},
+
+	toggleAll: function() {
+		todoList.toggleAll();
+		view.displayTodos();
+	}
+
+};
+
+var view = {
+	displayTodos: function() {
+		var todosUl = document.querySelector('ul');
+		todosUl.innerHTML = '';
+		for (var i = 0; i < todoList.todos.length; i++) {
+			var todoLi = document.createElement('li');
+			var todo = todoList.todos[i];
+			var todoTextWithCompletion = '';
+			
+			if (todo.completed === true) {
+				todoTextWithCompletion = '(x) ' + todo.todoText;
+			} else {
+				todoTextWithCompletion = '() ' + todo.todoText;
+			}		
+
+			todoLi.textContent = todoTextWithCompletion;	
+			todosUl.appendChild(todoLi);
+		}
+	}
+};
